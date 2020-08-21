cf3c697b98cfbef728a88e59adb23d7c288eb432
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-21 22:24:38 +0300

    Fetch data from api

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ if (navigator && navigator.geolocation) {
     function (position) {
       lat = position.coords.latitude;
       long = position.coords.longitude;
+
+      const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=9d79b17bcf6c72d27663f403013a283d`;
+
+      fetch(api)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+        });
     },
     function (error) {
       console.log(error);
